80c434b56550a3a473b7a0bbc1f6b09b3b3762c2
Daniel Ale <danielale9291@gmail.com>
2022-03-28 13:34:46 +0100

    update template

---
+++ src/shared/index.js
@@ -0,0 +1,11 @@
+import Button from './Button';
+import TextField from './TextInput';
+import Table from './Table';
+import Spinner from './Spinner';
+
+export {
+  Button,
+  TextField,
+  Table,
+  Spinner
+};
